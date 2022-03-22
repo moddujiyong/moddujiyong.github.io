e279b8ae3dcb6f76147adcf9595f0d8824ead445
Ji-Yong <cowjy0427@yonsei.ac.kr>
2022-03-23 00:27:30 +0900

    contact0323

--- src/pages/contact/Contact.js
+++ src/pages/contact/Contact.js
@@ -22,19 +22,26 @@ export default function contact() {
             </div>
 
             <div className="address">
-                .
+               <span className="address_title">Address</span>
+               <span className="address_content">B1, 20, Eonju-ro 63-gil, Gangnam-gu, Seoul, Republic of Korea</span>
             </div>
 
             <div className="email">
-            . 
+               <span className="email_title">Email</span>
+               <span className="email_content">mddu.lab.yonsei@gmail.com</span>
             </div>
 
             <div className="application">
-            .
+               <span className="application_title">Application</span>
+               <span className="application_content">Intern - Opening per semester </span>
+               <span className="application_content">Graduate students - No openings for now </span>
+               <span className="application_content">Requirements - CV & Academic Record (recommended) </span>
             </div>
 
             <div className="department">
-            .
+               <span className="department_title">Department</span>
+               <span className="department_content">Dept. of Medical Device Engineering & Management</span>
+               <span className="department_content"> http://mdi.yonsei.ac.kr/</span>
             </div>
         </div>
        </div>
