d45c9fb8d58861a355a6fb6b5aaa8e49f843722a
Ji-Yong <cowjy0427@kpu.ac.kr>
2022-04-07 16:54:40 +0900

    feedback0407_2

--- src/pages/members/intern/Intern.js
+++ src/pages/members/intern/Intern.js
@@ -17,22 +17,89 @@ export default function Intern() {
       <div className="intern-info" >
         
         
-        <span className="iilt">Sangeun Park (2018.09.01 ~ 2019.02.28) </span>
-        <span className="iil"> Dept. of Medical Device Engineering & Management, Graduate School, College of Medicine, Yonsei University</span>
-        <span className="iilt">Insik Im (2018.09.01 ~ 2018.12.31)</span>
-        <span className="iil"> Department of Medical Device Engineering & Management, Graduate School, College of Medicine, Yonsei University </span>
-        <span className="iilt">Kyungpil Kang (2018.10.01 ~ 2018.12.31) </span>
+        <span className="iilt">
+          Sangeun Park 
+          <span className="iild">(2018.09.01 ~ 2019.02.28)</span> 
+        </span>
         <span className="iil"> Department of Medical Device Engineering & Management, Graduate School, College of Medicine, Yonsei University</span>
-        <span className="iilt">Hanji Lee (2019.07-2020.02.28)</span>
+       
+        <span className="iilt">
+        Insik Im
+          <span className="iild">(2018.09.01 ~ 2018.12.31)</span> 
+        </span>
         <span className="iil"> Department of Medical Device Engineering & Management, Graduate School, College of Medicine, Yonsei University </span>
-        <span className="iilt">Jeonguk Choi (2019.07.01 ~ 2019.08.31)</span>
+
+        <span className="iilt">
+        Kyungpil Kang 
+          <span className="iild">(2018.10.01 ~ 2018.12.31)</span> 
+        </span>
+        <span className="iil"> Department of Medical Device Engineering & Management, Graduate School, College of Medicine, Yonsei University</span>
+
+        <span className="iilt">
+        Hanji Lee
+          <span className="iild">(2019.07-2020.02.28)</span> 
+        </span>
+        <span className="iil"> Department of Medical Device Engineering & Management, Graduate School, College of Medicine, Yonsei University</span>
+
+        <span className="iilt">
+        Jeonguk Choi
+          <span className="iild">(2019.07.01 ~ 2019.08.31)</span> 
+        </span>
         <span className="iil"> Department of Medical Devices Industry, Graduate School, College of Medicine, Yonsei University</span>
-        <span className="iilt">Dongwook Kim (2019.07.01 ~ 2019.08.31/ 2020.01.01~2020.02.28)</span>
-        <span className="iil"> Department of Biomedical Engineering, Yonsei University </span>
-        <span className="iilt">Choro Choi (2019.09.01 ~ 2020.08.31) </span>
+
+        <span className="iilt">
+        Dongwook Kim
+          <span className="iild">(2019.07.01 ~ 2019.08.31 / 2020.01.01~2020.02.28)</span> 
+        </span>
+        <span className="iil"> Department of Biomedical Engineering, Yonsei University</span>
+
+        <span className="iilt">
+        Choro Choi
+          <span className="iild">(2019.09.01 ~ 2020.08.31)</span> 
+        </span>
         <span className="iil"> Department of Medical Device Engineering & Management, Graduate School, College of Medicine, Yonsei University</span>
-        <span className="iilt">Minha Kim (2020.01.01 ~ 2020.02.28) </span>
-        <span className="iil"> Employment : CANDELA </span>
+
+        <span className="iilt">
+        Minha Kim 
+          <span className="iild">(2020.01.01 ~ 2020.02.28)</span> 
+        </span>
+        <span className="iil"> Employment : CANDELA</span>
+
+        <span className="iilt">
+        Hojoon Lee
+          <span className="iild">(2020.01.01 ~ 2020.02.28)</span> 
+        </span>
+        <span className="iil"> Department of Medical Device Engineering & Management, Graduate School, College of Medicine, Yonsei University</span>
+
+        <span className="iilt">
+        Heeyeong Choi
+          <span className="iild">(2020.07.01 ~ 2021.02.28)</span> 
+        </span>
+        <span className="iil">  Department of Medical Device Engineering & Management, Graduate School, College of Medicine, Yonsei University</span>
+
+        <span className="iilt">
+        Jeonghwa Lim
+          <span className="iild">(2020.07.01 ~ 2020.12.31)</span> 
+        </span>
+        <span className="iil">Department of Medical Device Engineering & Management, Graduate School, College of Medicine, Yonsei University </span>
+
+        <span className="iilt">
+        Sujin Lee
+          <span className="iild">(2020.07.01 ~ 2020.08.31)</span> 
+        </span>
+        <span className="iil"> Department of Biomedical Engineering, Yonsei University</span>
+
+        <span className="iilt">
+        Sehui Kim
+          <span className="iild"> (2020.07.01 ~ 2020.08.31)</span> 
+        </span>
+        <span className="iil"> RA specialist</span>
+
+       
+    
+
+       
+        
         <span className="iilt">Hojoon Lee (2020.01.01~2020.02.28)</span>
         <span className="iil"> Department of Biomedical Engineering, Yonsei University </span>
         <span className="iilt">Heeyeong Choi (2020.07.01 ~ 2021.02.28) </span>
@@ -41,7 +108,7 @@ export default function Intern() {
         <span className="iil"> Department of Medical Device Engineering & Management, Graduate School, College of Medicine, Yonsei University </span>
         <span className="iilt">Sujin Lee (2020.07.01 ~ 2020.08.31)</span>
         <span className="iil"> Department of Biomedical Engineering, Yonsei University </span>
-        <span className="iilt">Sehui Kim (2021.07.01 ~ 2020.08.31)</span>
+        <span className="iilt">Sehui Kim (2020.07.01 ~ 2020.08.31)</span>
         <span className="iil"> RA specialist</span>
        
 
