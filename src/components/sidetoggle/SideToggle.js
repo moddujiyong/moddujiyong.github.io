ca621af7965e75bf4026dff19451e86b53565c50
Ji-Yong <cowjy0427@kpu.ac.kr>
2022-04-22 17:16:27 +0900

    toggle study

--- src/components/sidetoggle/SideToggle.js
+++ src/components/sidetoggle/SideToggle.js
@@ -3,27 +3,19 @@ import {Link} from "react-router-dom";
 import "./sidetoggle.scss";
 
 
-const [isOpen, setMenu]=useState(false);
-
-const toggleMenu=()=>{
-    setMenu(isOpen=>!isOpen);
-}
-
-
 export default function SideToggle(){
-
     return (
-        <div className="side-toggle">
-            <div className='toggle-click' onClick={toggleMenu}>Click</div>
-            <div className="toggle-list">
-                <ul className="sideList">
-                     <li className="toggleListItem"><Link className="link" to ="/members" >Members</Link></li>
-                     <li className="toggleListItem"><Link className="link" to ="/research">Research</Link></li>
-                     <li className="toggleListItem"><Link className="link" to ="/publication">Publication</Link></li>
-                     <li className="toggleListItem"><Link className="link" to ="/contact">Contact</Link></li>
-                     <li className="toggleListItem"><Link className="link" to ="/gallery" >Gallery</Link></li>
-                </ul>
-            </div>
-        </div>
+      <div className="toggleCenter">
+         <ul className="toggleList">
+                    
+                    <li className="toggleListItem"><Link className="link" to ="/members" >Members</Link></li>
+                    <li className="toggleListItem"><Link className="link" to ="/research">Research</Link></li>
+                    <li className="toggleListItem"><Link className="link" to ="/publication">Publication</Link></li>
+                    <li className="toggleListItem"><Link className="link" to ="/contact">Contact</Link></li>
+                    <li className="toggleListItem"><Link className="link" to ="/gallery" >Gallery</Link></li>
+            </ul>
+      </div>
+           
+
     )
 }
--- src/components/top/Top.js
+++ src/components/top/Top.js
@@ -1,38 +1,36 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {IoIosSearch} from "react-icons/io";
 import "./top.scss";
 import {Link} from "react-router-dom";
 import Logo_Design_Eng from '../img/Logo_sizeAdj_new.png'
 import SideToggle from '../sidetoggle/SideToggle';
+import {AiOutlineBars} from 'react-icons/ai'
 export default function Top(){
-   /* <IoIosSearch className="topSearchIcon"/>
-    <div className="searchText">
-
-        Search
-      
-    </div>*/
+  //  {isOpen===true ? <SideToggle/> :null}
+ // <div className='toggle-click' onClick={()=>{setMenu(!isOpen)}}>
+//  <AiOutlineBars />
+ 
+    const [isOpen,setMenu]=useState(false);
     return(
         <div className="top">
+            
              <div className="topSet">
-                 
+            
                  <div className="topLeft">
                    <Link to="/"><img className="topImg" src={Logo_Design_Eng} alt=""></img> </Link> 
-                   
-                
+                     
                  </div>    
-                
-                <div className="topCenter">
-                
-                    <ul className="topList">
+                 <div className="topCenter">
+                 <ul className="topList">
                     
-                     <li className="topListItem"><Link className="link" to ="/members" >Members</Link></li>
-                     <li className="topListItem"><Link className="link" to ="/research">Research</Link></li>
-                     <li className="topListItem"><Link className="link" to ="/publication">Publication</Link></li>
-                     <li className="topListItem"><Link className="link" to ="/contact">Contact</Link></li>
-                     <li className="topListItem"><Link className="link" to ="/gallery" >Gallery</Link></li>
-                    </ul>
-                   
-                 </div> 
+                    <li className="topListItem"><Link className="link" to ="/members" >Members</Link></li>
+                    <li className="topListItem"><Link className="link" to ="/research">Research</Link></li>
+                    <li className="topListItem"><Link className="link" to ="/publication">Publication</Link></li>
+                    <li className="topListItem"><Link className="link" to ="/contact">Contact</Link></li>
+                    <li className="topListItem"><Link className="link" to ="/gallery" >Gallery</Link></li>
+                </ul>
+              </div>
+                
                
             </div>
         
