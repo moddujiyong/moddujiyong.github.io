abb5c1fea33105763447beea4aa3c9dd36f9ab74
Ji-Yong <cowjy0427@kpu.ac.kr>
2023-01-05 10:24:19 +0900

    hash add

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from 'react';
 import Top from './components/top/Top';
 import Home from './pages/home/Home';
 import Footer from './components/Footer/Footer';
-import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
-
+//import {BrowserRouter as Router, Switch, Route, Link} from "react-router-dom";
+import {HashRouter as Router, Switch, Route, Link} from "react-router-dom";
 import Gallery from './pages/gallery/Gallery';
 import Members from './pages/members/Members';
 import Research from './pages/research/Research';
@@ -25,7 +25,7 @@ function App() {
   return (
      <div className="App">
     
-       <Router >
+       <HashRouter >
          <>
             <Switch>
          
@@ -43,7 +43,7 @@ function App() {
             </Switch>
   
          </>
-       </Router>
+       </HashRouter>
 
     
     </div>
