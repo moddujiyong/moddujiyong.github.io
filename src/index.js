dea447bce67bfc5c5a24f26c5ba3df44869640cb
Ji-Yong <cowjy0427@kpu.ac.kr>
2023-01-05 10:21:08 +0900

    hash

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import {HashRouter} from 'react-router-dom'
 import App from './App';
 
-
+/*
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
+);*/
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>
+  </React.StrictMode>
 );
 
