238ff1c0df1014a772d1900fa847a8db7b393341
cho <hisstoryxx@gmail.com>
2023-02-27 16:40:06 +0900

    student adjust

--- pages/gallery/index.js
+++ pages/gallery/index.js
@@ -1,11 +1,17 @@
+import React, { useEffect, useRef } from "react";
 import Head from 'next/head'
 import Image from 'next/image'
 import { Inter } from '@next/font/google'
 import styles from '@/styles/Home.module.css'
-
+import {GLTFLoader} from 'three/examples/jsm/loaders/GLTFLoader'
+import * as THREE from 'three';
+import ThreeJS from "@/src/component/Three/Three";
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Gallery() {
+
+
+
   return (
     <div>
       <Head>
@@ -13,7 +19,17 @@ export default function Gallery() {
           Gallery | mddu
         </title>
       </Head>
+
+      {/* <div style = {{position : "relative", padding : "0 2rem", }}>
+        <div style = {{display:"flex", }}>
+        <ThreeJS />
+        </div>
+    
+   
+    </div>
+     */}
      
+      
     </div>
   )
 }
